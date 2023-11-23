aacaa137ca607ab2d39aceeb12689706137be7dd
Maestro-Kimpembe <115064419+Maestro-Kimpembe@users.noreply.github.com>
2023-11-23 22:03:03 +0100

    Add files via upload

---
+++ mandat.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int Kilometry;
+int main()
+{
+    cout << "Podaj o jaka liczbe przekroczyles predkosc:";
+    cin >> Kilometry;
+    if (Kilometry <= 10) {
+        cout << "Jestes winny 100zl mandatu";
+    }
+    if ((Kilometry > 10)&&(Kilometry < 30)) {
+        cout << "Jestes winny 200zl mandatu";
+    }
+    if (Kilometry > 30) {
+        cout << "Jestes winny 400zl mandatu";
+    }
+    return 0;
+}
+
