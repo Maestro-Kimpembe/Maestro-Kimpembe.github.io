bd4beeb9e91d6dc82a40407e81eb085fa82cc90d
Maestro-Kimpembe <115064419+Maestro-Kimpembe@users.noreply.github.com>
2023-12-09 15:25:16 +0100

    Create kalendarz.js

---
+++ kalendarz.js
@@ -0,0 +1,160 @@
+function fun() {
+  document.getElementById('btn1').style.display = "none";
+}
+function fun1() {
+  document.getElementById('btn2').style.display = "none";
+}
+function fun2() {
+  document.getElementById('btn3').style.display = "none";
+}
+function fun3() {
+  document.getElementById('btn4').style.display = "none";
+}
+function fun4() {
+  document.getElementById('btn5').style.display = "none";
+}
+function fun5() {
+  document.getElementById('btn6').style.display = "none";
+}
+function fun6() {
+  document.getElementById('btn7').style.display = "none";
+}
+function fun7() {
+  document.getElementById('btn8').style.display = "none";
+}
+function fun8() {
+  document.getElementById('btn9').style.display = "none";
+}
+function fun9() {
+  document.getElementById('btn10').style.display = "none";
+}
+function fun10() {
+  document.getElementById('btn11').style.display = "none";
+}
+function fun11() {
+  document.getElementById('btn12').style.display = "none";
+}
+function fun12() {
+  document.getElementById('btn13').style.display = "none";
+}
+function fun13() {
+  document.getElementById('btn14').style.display = "none";
+}
+function fun14() {
+  document.getElementById('btn15').style.display = "none";
+} function fun15() {
+  document.getElementById('btn16').style.display = "none";
+} function fun16() {
+  document.getElementById('btn17').style.display = "none";
+} function fun17() {
+  document.getElementById('btn18').style.display = "none";
+} function fun18() {
+  document.getElementById('btn19').style.display = "none";
+} function fun19() {
+  document.getElementById('btn20').style.display = "none";
+} function fun20() {
+  document.getElementById('btn21').style.display = "none";
+} function fun21() {
+  document.getElementById('btn22').style.display = "none";
+} function fun22() {
+  document.getElementById('btn23').style.display = "none";
+}
+function fun23() {
+  document.getElementById('btn24').style.display = "none";
+}
+function text() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML=" 1.Niech kazde otwarcie okienka przynosi ci cieplo i usmiech na twarzy."
+}
+function text1() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML=" 2.Niech drugi dzien przyniesie Ci blogoslawienstwo i milosc."
+}
+function text2() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="3.Trzeci dzien niech przyniesie Ci nadzieje i optymizm."
+}
+function text3() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="4.Czwarty dzien niech przyniesie Ci spokoj i harmonie."
+}
+function text4() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML=" 5.Piaty dzien niech przyniesie Ci radosc i usmiech."
+}
+function text5() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="6.Z okazji Mikolajek pragne zyczyc najlepszych prezentow."
+}
+function text6() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="7.Siodmy dzien niech przyniesie Ci szczescie i pomyslnosc."
+}
+function text7() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="8.Osmy dzien niech przyniesie Ci milosc i dobroc."
+}
+function text8() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="9.Dziewiaty dzien niech przyniesie Ci sile i determinacje do pokonywania przeszkod."
+}
+function text9() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="10.Dziesiaty dzien niech przyniesie Ci madrosc i zrozumienie."
+}
+function text10() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="11.Jedenasty dzien niech przyniesie Ci cierpliwosc i wytrwalosc."
+}
+function text11() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="12.Dwunasty dzien niech przyniesie Ci odwage i sile."
+}
+function text12() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="13.Trzynasty dzien niech przyniesie Ci spokoj i harmonie."
+}
+function text13() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="14.Zycze Ci wspanialego poczatku ostatniego czwartku przed Swietami"
+}
+function text14() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="15.W srodku tego zimowego szalenstwa, zycze Ci cieplych chwil i magicznego nastroju."
+}
+function text15() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="16.Z okazji soboty swiatecznej zycze Ci niezapomnianych chwil z najblizszymi."
+}
+function text16() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="17.Niech ten dzien przyniesie Ci wiele pozytywnych przezyc i wspanialych momentow z rodzina"
+}
+function text17() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="18.Zycze Ci spokojnego i relaksujacego poniedzialkowego wieczoru. "
+}
+function text18() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="19.Zycze Ci blogoslawionego dnia, pelnego spokoju i ciepla rodzinnego."
+}
+function text19() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="20.Niech sroda bedzie dla Ciebie pelna energii, pozytywnych zaskoczen i motywacji do dzialania"
+}
+function text20() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="21.Niech w Twoim sercu bedzie duzo swiatla, ciepla i radosci."
+}
+function text21() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="22.Zycze Ci chwil pelnych magii i niepowtarzalnego klimatu swiat, ktore juz coraz blizej."
+}
+function text22() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="23.Niech ten piatkowy wieczor przyniesie Ci wiele wzruszen."
+}
+function text23() {
+  document.getElementById('text').style.visibility="visible";
+document.getElementById('text').innerHTML="24.Zycze Ci jak najlepszego czasu spedzenie okresu Wigilii."
+}
