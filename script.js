8e11593b345220230e9a4464ef4c5c0867f185ab
Maestro-Kimpembe <115064419+Maestro-Kimpembe@users.noreply.github.com>
2023-12-10 16:09:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,455 @@
+function fun() {
+  document.getElementById('btn1').style.display = "none";
+}
+function fun1() {
+  document.getElementById('btn2').style.display = "none";
+}
+function fun2() {
+  document.getElementById('btn3').style.display = "none";
+}
+function fun3() {
+  document.getElementById('btn4').style.display = "none";
+}
+function fun4() {
+  document.getElementById('btn5').style.display = "none";
+}
+function fun5() {
+  document.getElementById('btn6').style.display = "none";
+}
+function fun6() {
+  document.getElementById('btn7').style.display = "none";
+}
+function fun7() {
+  document.getElementById('btn8').style.display = "none";
+}
+function fun8() {
+  document.getElementById('btn9').style.display = "none";
+}
+function fun9() {
+  document.getElementById('btn10').style.display = "none";
+}
+function fun10() {
+  document.getElementById('btn11').style.display = "none";
+}
+function fun11() {
+  document.getElementById('btn12').style.display = "none";
+}
+function fun12() {
+  document.getElementById('btn13').style.display = "none";
+}
+function fun13() {
+  document.getElementById('btn14').style.display = "none";
+}
+function fun14() {
+  document.getElementById('btn15').style.display = "none";
+} function fun15() {
+  document.getElementById('btn16').style.display = "none";
+} function fun16() {
+  document.getElementById('btn17').style.display = "none";
+} function fun17() {
+  document.getElementById('btn18').style.display = "none";
+} function fun18() {
+  document.getElementById('btn19').style.display = "none";
+} function fun19() {
+  document.getElementById('btn20').style.display = "none";
+} function fun20() {
+  document.getElementById('btn21').style.display = "none";
+} function fun21() {
+  document.getElementById('btn22').style.display = "none";
+} function fun22() {
+  document.getElementById('btn23').style.display = "none";
+}
+function fun23() {
+  document.getElementById('btn24').style.display = "none";
+}
+function text() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = " 1.Niech kazde otwarcie okienka przynosi ci cieplo i usmiech na twarzy."
+}
+function text1() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = " 2.Niech drugi dzien przyniesie Ci blogoslawienstwo i milosc."
+}
+function text2() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "3.Trzeci dzien niech przyniesie Ci nadzieje i optymizm."
+}
+function text3() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "4.Czwarty dzien niech przyniesie Ci spokoj i harmonie."
+}
+function text4() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = " 5.Piaty dzien niech przyniesie Ci radosc i usmiech."
+}
+function text5() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "6.Z okazji Mikolajek pragne zyczyc najlepszych prezentow."
+}
+function text6() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "7.Siodmy dzien niech przyniesie Ci szczescie i pomyslnosc."
+}
+function text7() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "8.Osmy dzien niech przyniesie Ci milosc i dobroc."
+}
+function text8() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "9.Dziewiaty dzien niech przyniesie Ci sile i determinacje do pokonywania przeszkod."
+}
+function text9() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "10.Dziesiaty dzien niech przyniesie Ci madrosc i zrozumienie."
+}
+function text10() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "11.Jedenasty dzien niech przyniesie Ci cierpliwosc i wytrwalosc."
+}
+function text11() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "12.Dwunasty dzien niech przyniesie Ci odwage i sile."
+}
+function text12() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "13.Trzynasty dzien niech przyniesie Ci spokoj i harmonie."
+}
+function text13() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "14.Zycze Ci wspanialego poczatku ostatniego czwartku przed Swietami"
+}
+function text14() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "15.W srodku tego zimowego szalenstwa, zycze Ci cieplych chwil i magicznego nastroju."
+}
+function text15() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "16.Z okazji soboty swiatecznej zycze Ci niezapomnianych chwil z najblizszymi."
+}
+function text16() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "17.Niech ten dzien przyniesie Ci wiele pozytywnych przezyc i wspanialych momentow z rodzina"
+}
+function text17() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "18.Zycze Ci spokojnego i relaksujacego poniedzialkowego wieczoru. "
+}
+function text18() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "19.Zycze Ci blogoslawionego dnia, pelnego spokoju i ciepla rodzinnego."
+}
+function text19() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "20.Niech sroda bedzie dla Ciebie pelna energii, pozytywnych zaskoczen i motywacji do dzialania"
+}
+function text20() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "21.Niech w Twoim sercu bedzie duzo swiatla, ciepla i radosci."
+}
+function text21() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "22.Zycze Ci chwil pelnych magii i niepowtarzalnego klimatu swiat, ktore juz coraz blizej."
+}
+function text22() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "23.Niech ten piatkowy wieczor przyniesie Ci wiele wzruszen."
+}
+function text23() {
+  document.getElementById('text').style.visibility = "visible";
+  document.getElementById('text').innerHTML = "24.Zycze Ci jak najlepszego czasu spedzenie okresu Wigilii."
+}
+function grajMuzyko() {
+  var muzyka = document.getElementById('klik');
+  muzyka.play();
+  muzyka.volume = 1.0;
+}
+var music = document.getElementById('klik2');
+music.volume = 0.5;
+function gulag1() {
+  document.getElementById('btn1')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 12) {
+    fun();
+    text3();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag2() {
+  document.getElementById('btn2')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 21) {
+    fun1();
+    text11();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag3() {
+  document.getElementById('btn3')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 11) {
+    fun2();
+    text10();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag4() {
+  document.getElementById('btn4')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 1) {
+    fun3();
+    text();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag5() {
+  document.getElementById('btn5')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 10) {
+    fun4();
+    text9();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag6() {
+  document.getElementById('btn6')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 2) {
+    fun5();
+    text1();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag7() {
+  document.getElementById('btn7')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 24) {
+    fun6();
+    text23();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag8() {
+  document.getElementById('btn8')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 23) {
+    fun7();
+    text22();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag9() {
+  document.getElementById('btn9')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 16) {
+    fun8();
+    text15();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag10() {
+  document.getElementById('btn10')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 13) {
+    fun9();
+    text12();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag11() {
+  document.getElementById('btn11')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 3) {
+    fun10();
+    text2();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag12() {
+  document.getElementById('btn12')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 5) {
+    fun11();
+    text4();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag13() {
+  document.getElementById('btn13')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 22) {
+    fun12();
+    text21();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag14() {
+  document.getElementById('btn14')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 18) {
+    fun13();
+    text17();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag15() {
+  document.getElementById('btn15')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 17) {
+    fun14();
+    text16();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag16() {
+  document.getElementById('btn16')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 6) {
+    fun15();
+    text5();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag17() {
+  document.getElementById('btn17')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 20) {
+    fun16();
+    text19();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag18() {
+  document.getElementById('btn18')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 7) {
+    fun17();
+    text6();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag19() {
+  document.getElementById('btn19')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 14) {
+    fun18();
+    text13();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag20() {
+  document.getElementById('btn20')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 21) {
+    fun19();
+    text20();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag21() {
+  document.getElementById('btn21')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 8) {
+    fun20();
+    text7();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag22() {
+  document.getElementById('btn22')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 9) {
+    fun21();
+    text8();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag23() {
+  document.getElementById('btn23')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 15) {
+    fun22();
+    text14();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
+function gulag24() {
+  document.getElementById('btn24')
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  if (currentDay === 19) {
+    fun23();
+    text18();
+    grajMuzyko();
+  } else {
+    window.location.href = "gulag.html";
+  }
+}
---
+++ script2.js
@@ -0,0 +1,10 @@
+function grajMuzyko() {
+  var muzyka = document.getElementById('klik');
+  muzyka.play();
+  muzyka.volume = 1.0;
+}
+function grajMuzyko1() {
+  var music = document.getElementById('klik3');
+  music.play();
+  muzyka.volume = 1.0;
+}
